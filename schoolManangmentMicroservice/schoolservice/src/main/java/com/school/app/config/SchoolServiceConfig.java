60c61d966ae6b6cbf15c870570cb263c2cfa85df
sfjbssept <sfjbssept@gmail.com>
2022-09-21 04:12:37 +0000

    added load balanecer

--- schoolManangmentMicroservice/schoolservice/src/main/java/com/school/app/config/SchoolServiceConfig.java
+++ schoolManangmentMicroservice/schoolservice/src/main/java/com/school/app/config/SchoolServiceConfig.java
@@ -4,10 +4,13 @@
 	import org.springframework.context.annotation.Bean;
 	import org.springframework.context.annotation.Configuration;
 	import org.springframework.web.client.RestTemplate;
+	import org.springframework.cloud.client.loadbalancer.*;
+	
 	
 	@Configuration
 	public class SchoolServiceConfig {
 	
+		@LoadBalanced
 		@Bean
 		public RestTemplate restTemplate() {
 			return new RestTemplate();
